359578d32b2076ad2bf1419700a3ccb6216bc9c0
btow <btow@yandex.ru>
2017-05-04 12:10:11 +0500

    Access from activity to fragments.

--- app/src/main/java/com/example/samsung/p1061_fragmentandactivity/MainActivity.java
+++ app/src/main/java/com/example/samsung/p1061_fragmentandactivity/MainActivity.java
@@ -4,9 +4,12 @@ import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Fragment fragment1;
     Fragment fragment2;
     FragmentTransaction fragmentTransaction;
 
@@ -19,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction = getFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.fragment2, fragment2);
         fragmentTransaction.commit();
+    }
+
+    public void onClickBtn(View view) {
+        String message = "Access to " + getString(R.string.fragment_1) + " from Activity";
+        fragment1 = getFragmentManager().findFragmentById(R.id.fragment1);
+        ((TextView) fragment1.getView().findViewById(R.id.tvFragment)).setText(message);
+
+        message = "Access to " + getString(R.string.fragment_2) + " from Activity";
+        ((TextView) fragment2.getView().findViewById(R.id.tvFragment)).setText(message);
     }
 }
